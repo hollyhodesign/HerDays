343415fbdbaead2318f4df5dab817277e1edc374
hollyhodesign <hollyhodesign@gmail.com>
2026-05-04 12:32:17 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // 新增這一行
+  root: 'src', // 新增這一行，告訴 Vite 從 src 開始找 index.html
+  base: './',
+  build: {
+    outDir: '../dist', // 因為 root 改了，輸出路徑要往上一層
+  }
 })
